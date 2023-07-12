daadf9b37d13bc2e37abf89e5a04e1776b59887f
Jax-Hax <94689952+Jax-Hax@users.noreply.github.com>
2023-07-11 20:16:37 -0400

    adding guaranteed path to villages

--- src/generation.rs
+++ src/generation.rs
@@ -5,12 +5,13 @@ use ggez::{
 };
 use crate::structs::*;
 //Constants
-const MAP_X: usize = 20;
-const MAP_Y: usize = 20;
+const MAP_X: usize = 20; //This number unfortunately has to be fixed as I couldn't wrap my head around calculating everything for any possible number
+const MAP_Y: usize = 20; //This number unfortunately has to be fixed as I couldn't wrap my head around calculating everything for any possible number
 const MAGNIFICATION: f64 = 10.0;
 //What numbers generate water and what generates mountains. The rest of the tiles are land tiles.
 const WATER_VALUES: [f64; 2] = [0.15,-0.1]; //less than value 1 and greater than value 2
 const MOUNTAIN_VALUES: [f64; 2] = [-0.5,-1.0]; //less than value 1 and greater than value 2
+//The different splits of the tilemap, 4 squares on each side with a gap in the middle and a middle village
 
 pub fn generate_tilemap( seed: u32 ) -> [[TileType; MAP_X]; MAP_Y]{
     //Generates a new perlin seed and gets a value from it for each tile, based on magnification. It then checks to see what tile type it should be.
@@ -27,16 +28,15 @@ pub fn generate_tilemap( seed: u32 ) -> [[TileType; MAP_X]; MAP_Y]{
             }
         }
     }
-    //TODO: set the starting area to be land, this code doesnt work
-    /*for row in 0..3{
-        for col in 0..3{
-            println!("{},{}",row,col);
-            tile_array[row+3][col+3] = TileType::Land; //top left
-            tile_array[row+3][MAP_X-3] = TileType::Land; //top right
+    //Make a guaranteed path to the villages
+    //you start at two spaces away from your corner, so top left would be [2,2]
+    //then there is a village on you, a village 4 spaces to both sides, and a village diagonally towards the middle 4 spaces away, making a square of villages that is 6x6
+    for row in 0..4{
+        tile_array[2+row][2] = TileType::Land; //top left
+            /*tile_array[row+3][MAP_X-3] = TileType::Land; //top right
             tile_array[MAP_Y-3][col+3] = TileType::Land; //bottom left
-            tile_array[MAP_Y-3][MAP_X-3] = TileType::Land; //bottom right
-        }
-    }*/
+            tile_array[MAP_Y-3][MAP_X-3] = TileType::Land; //bottom right*/
+    }
     tile_array
 }
 pub fn generate_tilemap_mesh(ctx: &Context, tilemap: [[TileType; MAP_X]; MAP_Y]) -> Mesh{
@@ -58,4 +58,12 @@ pub fn generate_tilemap_mesh(ctx: &Context, tilemap: [[TileType; MAP_X]; MAP_Y])
         }
     }
     Mesh::from_data(ctx, mesh_builder.build())
+}
+/*pub fn generate_resource_map(tilemap: &mut [[TileType; MAP_X]; MAP_Y], seed: u32) -> [[ResourceType; MAP_X]; MAP_Y]{
+    let num_villages = 
+
+
+}*/
+fn get_random_resource(tile_type: TileType,resources_left: u32){
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
 use ggez::{
     conf::{WindowMode, WindowSetup},
     event::{self, MouseButton},
-    graphics::{self, Canvas, Color, DrawMode, DrawParam, Image, Mesh, MeshBuilder, Rect},
+    graphics::{self, Canvas, DrawMode, DrawParam, Image, Mesh, MeshBuilder, Rect},
     Context, GameResult,
 };
 use std::path;
 mod structs;
 use structs::*;
 mod generation;
-use generation::*;
 const MAP_X: usize = 20;
 const MAP_Y: usize = 20;
 struct GameState{
     tilemap: [[TileType; MAP_X]; MAP_Y],
-    tilemap_mesh: Mesh
+    tilemap_mesh: Mesh,
+    // Store the camera position
+    camera_position: ggez::mint::Point2<f32>,
+    frames: usize,
 }
 fn main() -> GameResult {
     let resource_dir = path::PathBuf::from("./resources");
@@ -30,9 +32,10 @@ fn main() -> GameResult {
 }
 impl GameState{
     fn new(ctx: &mut Context) -> GameResult<GameState> {
-        let tilemap = generation::generate_tilemap(2);
+        let mut tilemap = generation::generate_tilemap(2);
         let tilemap_mesh = generation::generate_tilemap_mesh(ctx, tilemap);
-        let state = GameState{tilemap, tilemap_mesh};
+        //let resource_map = generation::generate_resource_map(&mut tilemap,2);
+        let state = GameState{tilemap, tilemap_mesh,camera_position: ggez::mint::Point2 { x: 0.0, y: 0.0 },frames: 0};
         Ok(state)
     }
 }
@@ -45,6 +48,10 @@ impl event::EventHandler<ggez::GameError> for GameState {
         let mut canvas = Canvas::from_frame(ctx, graphics::Color::from([0.1, 0.2, 0.3, 1.0]));
         draw_game(&mut canvas, self);
         canvas.finish(ctx)?;
+        self.frames += 1;
+        if (self.frames % 100) == 0 {
+            println!("FPS: {}", ctx.time.fps());
+        }
         Ok(())
     }
     fn mouse_button_down_event(
@@ -57,9 +64,6 @@ impl event::EventHandler<ggez::GameError> for GameState {
         match btn {
             MouseButton::Left => {
                 //Left click
-            }
-            MouseButton::Right => {
-                //Right click
             }
             _ => (),
         }
@@ -81,7 +85,22 @@ impl event::EventHandler<ggez::GameError> for GameState {
         }
         Ok(())
     }
+    fn mouse_motion_event(
+        &mut self,
+        ctx: &mut Context,
+        _x: f32,
+        _y: f32,
+        dx: f32,
+        dy: f32,
+    ) -> GameResult {
+        if ctx.mouse.button_pressed(MouseButton::Right) {
+            self.camera_position.x += dx;
+            self.camera_position.y += dy;
+        }
+        Ok(())
+    }
+    
 }
 fn draw_game(canvas: &mut Canvas, state: &mut GameState){
-    canvas.draw(&state.tilemap_mesh, DrawParam::default());
+    canvas.draw(&state.tilemap_mesh, DrawParam::dest(DrawParam::default(),state.camera_position));
 }
